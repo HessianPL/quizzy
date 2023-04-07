e129492502d781339c5febcb1d20bf05900c335b
HessianPL <git@lukaszdragon.pl>
2023-04-07 20:50:14 +0200

    Fix bug in routes in App component - change element rendered from QuizMaker component to QuizMakerView view

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {AboutView} from "./views/AboutView";
 import {HomeView} from "./views/HomeView";
 import {QuizMaker} from "./components/QuizMaker/QuizMaker";
 import {NotFoundView} from "./views/NotFoundView";
+import {QuizMakerView} from "./views/QuizMakerView";
 
 function App() {
     console.log('Testing App')
@@ -15,7 +16,7 @@ function App() {
       <Header />
         <Routes>
             <Route path="/" element={<HomeView />} />
-            <Route path="/makequiz" element={<QuizMaker />} />
+            <Route path="/makequiz" element={<QuizMakerView />} />
             <Route path="/about" element={<AboutView />} />
             <Route path="/*" element={<NotFoundView />} />
         </Routes>
