47f61dbe7adbd223ea6023006842c305deea0087
HessianPL <git@lukaszdragon.pl>
2023-04-10 22:25:24 +0200

    Add new quiz form (without adding questions yet, only quiz data)

--- src/components/QuizMaker/QuizMaker.tsx
+++ src/components/QuizMaker/QuizMaker.tsx
@@ -1,8 +1,59 @@
+import {ChangeEvent, SyntheticEvent, useState} from "react";
+import { QuizEntityIncoming } from "../../../../quizzy-back/types/quiz/quiz-entity-incoming";
+import './QuizMaker.css'
+
 export const QuizMaker = () => {
+    const [newQuizData, setNewQuizData] = useState<QuizEntityIncoming>({
+            title: '',
+            description: '',
+            passingPercentage: 100,
+            timerQuiz: 0,
+            instantFeedback: true,
+            endingFeedback: false,
+            publicListing: false,
+            passwordProtected: false,
+        }
+    )
+
+    const handleSubmit = (event: SyntheticEvent) => {
+        event.preventDefault()
+        console.log('Form submission')
+        console.log(newQuizData)
+    }
+
+    type InputValue = string | boolean;
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const target = event.target;
+        const inputValue: InputValue =
+            target instanceof HTMLInputElement && target.type === 'checkbox'
+                ? target.checked
+                : target.value;
+
+        setNewQuizData(prevNewQuizData => {
+            return {
+                ...prevNewQuizData,
+                [target.name]: inputValue
+            }
+        })
+    }
+
     return (
         <>
-            <p>This is a quiz maker component</p>
-            <p>Add a possibility to create new quizzes here</p>
+            <h2>Create your own quiz!</h2>
+            <form onSubmit={handleSubmit} className='newQuizForm'>
+                <label htmlFor='title'>Quiz title:</label>
+                <input type="text" name="title" id='title' placeholder="Title of the quiz" value={newQuizData.title} onChange={handleChange}/>
+                <label>Description: <textarea name="description" placeholder="Description" value={newQuizData.description} onChange={handleChange}/></label>
+                <label>Passing percentage: <input type="number" name="passingPercentage" min={0} max={100} value={newQuizData.passingPercentage} onChange={handleChange} /></label>
+                <label>Time to complete (max 120 minutes): <input type="number" name="timerQuiz" min={0} max={60*60*2} value={newQuizData.timerQuiz} onChange={handleChange} /></label>
+                <label>Instant feedback: <input type="checkbox" name="instantFeedback" checked={newQuizData.instantFeedback} onChange={handleChange} /></label>
+                <label>Ending feedback: <input type="checkbox" name="endingFeedback" checked={newQuizData.endingFeedback} onChange={handleChange} /></label>
+                <label>Public listing: <input type="checkbox" name="publicListing" checked={newQuizData.publicListing} onChange={handleChange} /></label>
+
+                <button>Save quiz!</button>
+            </form>
         </>
     )
 }
+
