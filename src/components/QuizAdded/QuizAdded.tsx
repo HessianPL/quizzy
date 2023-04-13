5221e7b5d9670bb0fcd2d2fa6e58797cfeae9667
HessianPL <git@lukaszdragon.pl>
2023-04-13 12:29:34 +0200

    Change CSS for QuizAdded component and add informational about public visibility of quiz

--- src/components/QuizAdded/QuizAdded.tsx
+++ src/components/QuizAdded/QuizAdded.tsx
@@ -7,9 +7,10 @@ interface Props {
 export const QuizAdded = (props: Props) => {
     const quizURL: string =`/quiz/${props.quizID}`
     return (
-        <>
+        <div className='quizAddedInfo'>
             <h3>The quiz has been added!</h3>
             <p>You may find it with this link: <a href={quizURL}>{quizURL}</a></p>
-        </>
+            <p>Additionally, since you indicated that the quiz should be publicly visible, you will find it in the home tab, on the list of publicly visible quizzes.</p>
+        </div>
     )
 }
